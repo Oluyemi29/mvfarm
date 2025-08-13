e7f74707b19a3d43d12e38b105cba6861ca24830
Oluyemi29 <adedokunoluyemi29@gmail.com>
2025-08-13 22:09:37 +0100

    image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     url: "https://mvfarm.vercel.app",
     siteName: "MV Farm",
     images: {
-      url: "/tempmvfarm.png",
+      url: "/tempmvfarm.jpg",
       width: 1200,
       height: 630,
       alt: "MV Farm",
